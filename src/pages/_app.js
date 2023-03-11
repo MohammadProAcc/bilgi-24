7ff4ae2cef1b47ccc62dcee8d731d50c189cda5a
MohammadProAcc <mohammadsprofessionalemail@gmail.com>
2023-03-11 22:24:42 +0330

    add mui support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "@mui/material";
+import { theme, createEmotionCache } from "@/lib";
+import "@/styles/globals.css";
+import { CacheProvider } from "@emotion/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={createEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
