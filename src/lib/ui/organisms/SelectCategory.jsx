2b5061598133fff657b593a591396f0b8ff774dd
MohammadProAcc <mohammadsprofessionalemail@gmail.com>
2023-03-12 13:57:25 +0330

    replace material-next example

--- src/lib/ui/organisms/SelectCategory.jsx
+++
@@ -1,3 +0,0 @@
-export function SelectCategory() {
-  return <$>SelectCategory</$>;
-}
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import { HomePage } from '@/lib/ui'
-
-export default function Home() {
-  return <HomePage />
-}
