1649eb9a8772417dd8ae4bc350d22b9e0d386cb6
MohammadProAcc <mohammadsprofessionalemail@gmail.com>
2023-03-21 12:17:18 +0330

    add NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import Head from "next/head";
 import PropTypes from "prop-types";
+import NextProgress from "next-progress";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
@@ -11,6 +12,7 @@ import "swiper/css/pagination";
 import "swiper/css/thumbs";
 import "react-image-gallery/styles/css/image-gallery.css";
 import "../styles/global.css";
+import { Color } from "$/design";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -26,6 +28,11 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <NextProgress
+          delay={300}
+          options={{ showSpinner: false }}
+          color={Color.sub}
+        />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
